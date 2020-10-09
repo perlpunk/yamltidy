a7892e6a196443f50fe2ab93cf038e31bff5bcb4
Tina Müller <cpan2@tinita.de>
2020-10-09 16:42:54 +0200

    Restructure data/html generation

---
+++ etc/utils/generate.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use v5.20;
+use experimental qw/ signatures /;
+use autodie qw/ open opendir mkdir /;
+use FindBin '$Bin';
+use lib "$Bin/../../lib";
+
+use File::Copy qw/ copy /;
+use YAML::Tidy;
+$|++;
+
+my $datadir = "$Bin/generated";
+mkdir $datadir unless -d $datadir;
+my $ts = "$Bin/../../yts";
+opendir(my $dh, $ts);
+my @ids = grep { m/^[A-Z0-9]{4}$/ } readdir $dh;
+closedir $dh;
+
+my @skip;
+open my $fh, '<', "$Bin/../../t/libyaml.skip";
+chomp(@skip = <$fh>);
+close $fh;
+@skip = grep {
+    length $_ and not m/^ *#/
+} @skip;
+
+my %skip;
+@skip{ @skip } = (1) x @skip;
+my @valid;
+for my $id (sort @ids) {
+    next if -e "$ts/$id/error";
+    next if $skip{ $id };
+    push @valid, $id;
+}
+
+my %types = (
+    indent => {
+        ids => \@valid,
+        configs => [0 .. 3],
+    },
+);
+
+my @yt = map {
+    my $cfg = YAML::Tidy::Config->new( configfile => "$Bin/../../t/data/configs/config$_.yaml" );
+    YAML::Tidy->new( cfg => $cfg );
+} (0 .. 3);
+
+for my $type (sort keys %types) {
+    my $def = $types{ $type };
+    my $ids = $def->{ids};
+    my $configs = $def->{configs};
+    generate($type, $ids, $configs);
+}
+
+sub generate($type, $ids, $configs) {
+    say "\n==== $type";
+    my $dir = "$datadir/$type";
+    mkdir $dir unless -d $dir;
+
+    for my $id (@$ids) {
+        print "\r=========== $id";
+        my $in = "$ts/$id/in.yaml";
+        my $dir = "$dir/$id";
+        mkdir $dir unless -d $dir;
+        copy $in, "$dir/in.yaml";
+        open(my $fh, '<:encoding(UTF-8)', $in);
+        my $yaml = do { local $/; <$fh> };
+        close $fh;
+        for my $i (@$configs) {
+            my $yt = $yt[ $i ];
+            my $out = $yt->tidy($yaml);
+            open my $fh, '>:encoding(UTF-8)', "$dir/c$i.yaml";
+            print $fh $out;
+            close $fh;
+        }
+    }
+}
+say "\ndone";
---
+++ etc/utils/html.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use v5.20;
+use experimental qw/ signatures /;
+use autodie qw/ open opendir mkdir /;
+use FindBin '$Bin';
+use lib "$Bin/../../lib";
+
+use YAML::PP::Highlight;
+use YAML::PP::Parser;
+use YAML::Tidy;
+
+my @configs = map {
+    open my $fh, '<', "$Bin/../../t/data/configs/config$_.yaml";
+    my $yaml = do { local $/; <$fh> };
+    close $fh;
+    my $html = YAML::Tidy->highlight($yaml, 'html');
+    $html;
+} (0 .. 3);
+
+$|++;
+my %types = (
+    indent => {
+        configs => [0 .. 3],
+    },
+);
+
+for my $type (sort keys %types) {
+    my $datadir = "$Bin/generated/$type";
+    opendir(my $dh, $datadir);
+    my @ids = sort grep { m/^[A-Z0-9]{4}$/ } readdir $dh;
+    closedir $dh;
+    html($type, \@ids, [0 .. 3]);
+}
+
+sub html($type, $ids, $configs) {
+    my $table = qq{<table class="highlight">};
+    $table .= qq{<tr><th>ID</th><th><span class="ytitle">Input</span> / <span class="xtitle">Config</span></th>};
+    $table .= qq{<th align="left"><pre>$configs[ $_ ]</th>\n} for @$configs;
+    $table .= qq{</tr>};
+    for my $id (@$ids) {
+        print "\r======== $id";
+        my $datadir = "$Bin/generated/$type";
+        my @configs = map { "c$_" } @$configs;
+        my @names = ('in', @configs);
+        $table .= qq{<tr><td class="id" id="id$id"><pre><b><a href="#id$id">$id</a></b></pre></td>};
+        for my $i (0 .. $#names) {
+            my $name = $names[ $i ];
+            my $file = "$datadir/$id/$name.yaml";
+            open my $fh, '<:encoding(UTF-8)', $file;
+            my $yaml = do { local $/; <$fh> };
+            close $fh;
+            my $html = YAML::Tidy->highlight($yaml, 'html');
+            my $class = 'yaml';
+            $class .= ' input' if $name eq 'in';
+            $table .= qq{<td class="$class"><pre>$html</pre></td>\n};
+        }
+        $table .= qq{</tr>\n};
+    }
+    say "\ndone";
+    $table .= qq{</table>};
+
+    my $html = <<"EOM";
+<html>
+<head>
+<title>YAML Tidy Examples - Group $type</title>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/yaml.css">
+<body>
+$table
+</body>
+</html>
+EOM
+    open my $fh, '>:encoding(UTF-8)', "$Bin/../../etc/html/$type.html";
+    print $fh $html;
+    close $fh;
+}
